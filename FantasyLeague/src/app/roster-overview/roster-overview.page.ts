894161e4caebabb48c1e1792420a59116ad1336c
sem15 <43047472+sem15@users.noreply.github.com>
2020-04-30 15:06:33 -0400

    Create roster-overview.page.ts

---
+++ FantasyLeague/src/app/roster-overview/roster-overview.page.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import {Router,ActivatedRoute} from '@angular/router';
+import {RosterService} from '../roster.service';
+@Component({
+  selector: 'app-roster-overview',
+  templateUrl: './roster-overview.page.html',
+  styleUrls: ['./roster-overview.page.scss'],
+})
+export class RosterOverviewPage implements OnInit {
+
+  currentroster:any;
+  constructor(
+    private router: Router,
+    private rosterService: RosterService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.currentroster=this.rosterService.param;
+    console.log(this.rosterService.param);
+  }
+
+  updateStatus(players){
+    this.rosterService.id=this.currentroster.id;
+    // this.rosterService.players=this.currentroster.players;
+    // console.log("players:"+this.rosterService.players);
+    // console.log("currentrosterplayers:"+this.currentroster.players);
+    this.router.navigate(["/editroster",players])
+  }
+
+}
