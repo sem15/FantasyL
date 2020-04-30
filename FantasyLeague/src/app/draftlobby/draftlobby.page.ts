e925549a572ac8b1fc403b2a2252b266c64bec2d
sem15 <sem15@email.sc.edu>
2020-04-30 11:57:09 -0400

    update

--- FantasyLeague/src/app/draftlobby/draftlobby.page.ts
+++ FantasyLeague/src/app/draftlobby/draftlobby.page.ts
@@ -3,6 +3,7 @@ import { Validators, FormBuilder, FormControl, FormGroup } from '@angular/forms'
 import { Router } from '@angular/router';
 import { LeagueService } from '../league.service';
 import { RosterService } from '../roster.service';
+import {PlayerService} from'../player.service';
 import * as firebase from 'firebase';
 
 @Component({
@@ -13,14 +14,30 @@ import * as firebase from 'firebase';
 export class DraftlobbyPage implements OnInit {
 
   new_draft_form: FormGroup;
+  topList:Array<any>=[];
+  midList:Array<any>=[];
+  jngList:Array<any>=[];
+  adcList:Array<any>=[];
+  supList:Array<any>=[];
+  playersList:Array<any>=[];
 
 
   constructor(
     private router: Router,
     public formBuilder: FormBuilder,
     public leagueService: LeagueService,
-    public rosterService: RosterService
-  ) { }
+    public rosterService: RosterService,
+    private playerService:PlayerService
+  ) {
+    this.playerService.getPlayers();
+    this.playersList = this.playerService.getPlayers();
+    console.log("got " + this.playersList.length + " players loaded.");
+    for (let i = 0; i < this.playersList.length; i++) {
+      if(this.playersList[i].Position == "Top") {
+        console.log(this.playersList[i].IGN + this.playersList[i].Position);
+      }
+    }
+  }
 
   ngOnInit() {
     this.new_draft_form = this.formBuilder.group({
